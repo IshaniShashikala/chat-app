7e23c4fdcc90b347bf38ac780939f9739d89a2c9
Ishani Thembuwana <ishaniist@gmail.com>
2022-09-11 20:54:14 +0530

    Add Event Acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,9 +8,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
-    // const message = document.querySelector('input').value
 
-    socket.emit('sendMessage', message)
+    // socket.emit('sendMessage', message, (message) => {
+    //     console.log('The message was delivered!', message)
+    // })
+    socket.emit('sendMessage', message, (error) => {
+       if (error) {
+         return console.log(error)
+       }
+       console.log('The message was delivered!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () =>{
@@ -23,6 +30,8 @@ document.querySelector('#send-location').addEventListener('click', () =>{
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
         })
     })
 })
