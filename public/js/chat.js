3675c60488084499c3d31d53fb3a27c96aacda1f
Ishani Thembuwana <ishaniist@gmail.com>
2022-09-12 22:47:33 +0530

    Add Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,29 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationBUtton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    //disable
 
     const message = e.target.elements.message.value
 
-    // socket.emit('sendMessage', message, (message) => {
-    //     console.log('The message was delivered!', message)
-    // })
     socket.emit('sendMessage', message, (error) => {
+        //enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
        if (error) {
          return console.log(error)
        }
@@ -20,10 +31,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () =>{
+$sendLocationBUtton.addEventListener('click', () =>{
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $sendLocationBUtton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position)
@@ -31,6 +44,7 @@ document.querySelector('#send-location').addEventListener('click', () =>{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationBUtton.removeAttribute('disabled')
             console.log('Location shared!')
         })
     })
