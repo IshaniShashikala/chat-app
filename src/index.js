a2aa7fc979e1ed4bd21bceb5ad28a251c00a1193
Ishani Thembuwana <ishaniist@gmail.com>
2022-09-18 12:23:51 +0530

    Use Socket.io Rooms

--- src/index.js
+++ src/index.js
@@ -17,12 +17,17 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) =>{
     console.log('New Websocket Connection')
 
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
+
     // socket.emit('message', {
     //     text: 'Welcome!',
     //     createdAt: new Date().getTime()
     // })
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -31,7 +36,7 @@ io.on('connection', (socket) =>{
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', generateMessage(message))
+        io.to('Center City').emit('message', generateMessage(message))
         callback()
         // callback('Delivered!')
     })
